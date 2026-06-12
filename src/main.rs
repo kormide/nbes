92dfc7976c2a6ccd05b65c0e4c2f1a97ca89336c
Derek Cormier <derek.m.cormier@gmail.com>
2026-06-12 16:08:19 -0700

    feat: forward build tool events to bes backends

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use anyhow::{Context, Result};
 use args::Args;
 use build_proto::google::devtools::build::v1::{
-    OrderedBuildEvent, PublishBuildToolEventStreamRequest, PublishBuildToolEventStreamResponse,
+    PublishBuildToolEventStreamRequest, PublishBuildToolEventStreamResponse,
     PublishLifecycleEventRequest,
     publish_build_event_client::PublishBuildEventClient,
     publish_build_event_server::{PublishBuildEvent, PublishBuildEventServer},
 };
 use clap::Parser;
 use futures::{Stream, stream::unfold};
+use futures::{
+    future::join_all,
+    stream::{self, StreamExt},
+};
 use std::{env, pin::Pin, str::FromStr};
+use tokio::sync::broadcast;
+use tokio_stream::wrappers::BroadcastStream;
 use tonic::{
     Request, Response, Status, Streaming,
     transport::{Channel, Endpoint, Server},
@@ -67,44 +73,90 @@ impl PublishBuildEvent for NBesService {
         request: Request<Streaming<PublishBuildToolEventStreamRequest>>,
     ) -> Result<Response<PublishBuildToolEventStreamStream>, Status> {
         struct State {
-            incoming: Streaming<PublishBuildToolEventStreamRequest>,
+            /// Incoming request stream from Bazel
+            incoming_requests: Streaming<PublishBuildToolEventStreamRequest>,
+            /// Incoming response streams from the bes backends
+            incoming_responses: Vec<Streaming<PublishBuildToolEventStreamResponse>>,
         }
 
+        // Open streams to each of the bes backends. Incoming requests are sent
+        // via a broadcast channel. Each backend turns its receiver into a stream
+        // to forward requests to the backend.
+        let (tx, _) = broadcast::channel::<PublishBuildToolEventStreamRequest>(256);
+        let incoming_responses = stream::iter(self.clients.iter())
+            .then(|client| async {
+                let receiver = tx.subscribe();
+                let outbound_requests = BroadcastStream::new(receiver).map(|request| {
+                    // BroadcastStream wraps the request in a Result in case it fails to
+                    // receive from the sender. But publish_build_tool_event_stream() takes
+                    // a stream of requests, so we must unwrap it. This should "never" occur,
+                    // so just panic.
+                    request.expect("failed to receive request from broadcast channel")
+                });
+                client
+                    .clone() // cloning clients is cheap
+                    .publish_build_tool_event_stream(Request::new(outbound_requests))
+                    .await
+                    .expect("failed to initiate stream")
+                    .into_inner()
+            })
+            .collect()
+            .await;
+
         let state = State {
-            incoming: request.into_inner(),
+            incoming_requests: request.into_inner(),
+            incoming_responses,
         };
 
-        Ok(Response::new(Box::pin(unfold(
-            state,
-            |mut state| async move {
-                let msg = state
-                    .incoming
+        Ok(Response::new(Box::pin(unfold(state, move |mut state| {
+            let tx = tx.clone();
+            async move {
+                // Receive a request from the Bazel client
+                let request = state
+                    .incoming_requests
                     .message()
                     .await
                     .expect("failed to receive message");
 
-                if let Some(PublishBuildToolEventStreamRequest {
-                    ordered_build_event:
-                        Some(OrderedBuildEvent {
-                            stream_id,
-                            sequence_number,
-                            ..
-                        }),
-                    ..
-                }) = msg
-                {
+                if let Some(request) = request {
+                    // Forward the request to all backends via the broadcast channel
+                    if !state.incoming_responses.is_empty() {
+                        tx.send(request.clone()).expect("failed to send message");
+                    }
+
+                    // Wait for a response from each backend
+                    let responses: Vec<_> =
+                        join_all(state.incoming_responses.iter_mut().map(|r| r.message()))
+                            .await
+                            .into_iter()
+                            .map(|r| r.expect("failed to receive message from backend"))
+                            .collect();
+
+                    // Validate the responses
+                    for response in responses {
+                        match response {
+                            Some(_) => {
+                                // TODO: validate received expected response
+                            }
+                            None => {
+                                todo!("unexpected end of stream")
+                            }
+                        };
+                    }
+
+                    // Send a single response back
+                    let build_event = request.ordered_build_event.expect("TODO");
                     return Some((
                         Ok(PublishBuildToolEventStreamResponse {
-                            stream_id,
-                            sequence_number,
+                            stream_id: build_event.stream_id,
+                            sequence_number: build_event.sequence_number,
                         }),
                         state,
                     ));
                 }
-
                 None
-            },
-        ))))
+            }
+        }))))
     }
 
     async fn publish_lifecycle_event(
